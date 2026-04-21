b92f818683f588734bb47b5a098725f4ec1c08f1
Talal Umar <miantalal251@gmail.com>
2026-04-21 20:36:06 +0500

    Setup full stack project with backend and frontend

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+﻿export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Resume Job Matcher
+      </h1>
+    </main>
+  );
+}
